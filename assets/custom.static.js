5a8d50df533c8a3845bca9ef761c2e46f534daaf
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2022-11-21 22:49:00 +0600

    added brand slider

--- assets/custom.static.js
+++ assets/custom.static.js
@@ -1,24 +1,21 @@
-console.log("does it work?");
-
 window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
   const imageSlider = new Swiper(".product-images-swiper", {
-    // Optional parameters
-    // spaceBetween: 0,
-
     loop: true,
     autoHeight: true,
-
-    // enable auto height
-    // If we need pagination
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
-    // Navigation arrows
     navigation: {
       nextEl: ".product-images-swiper .swiper-button-next",
       prevEl: ".product-images-swiper .swiper-button-prev",
+    },
+  });
+  new Swiper(".brand-images-swiper", {
+    loop: true,
+    autoHeight: true,
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: ".brand-images-swiper .swiper-button-next",
+      prevEl: ".brand-images-swiper .swiper-button-prev",
     },
   });
 });
